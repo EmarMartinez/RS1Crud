e4121fa79c043f0265a2593a7a937d5b16a74a01
Emar Martinez <emar.martinez@bosonit.com>
2022-07-18 10:19:02 +0200

    ejercicio acabado

---
+++ src/main/java/com/bosonit/RS1Crud/PersonaService.java
@@ -0,0 +1,32 @@
+package com.bosonit.RS1Crud;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface PersonaService {
+    String getNombre();
+    String getPoblacion();
+    int getEdad();
+    int getID();
+
+    void setNombre(String nombre);
+    void setPoblacion(String poblacion);
+    void setEdad(int edad);
+    void setId(int id);
+
+    List<Persona> addPersona(Persona persona);
+
+    Persona modifyPersona(Persona persona, int id);
+
+    boolean existeById(int id);
+
+    List<Persona> listaPersonas();
+
+    Persona borraPersona(int id);
+
+    List<Persona> buscarPorNombre(String nombre);
+
+    Persona buscaPersona(int id);
+}
---
+++ src/main/java/com/bosonit/RS1Crud/Rs1CrudApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.RS1Crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rs1CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rs1CrudApplication.class, args);
+	}
+
+}
